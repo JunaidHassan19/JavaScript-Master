1ac53a9f09a2d1ee3636d04227a143af0f4921f7
Junaid Hassan <123434178+JunaidHassan19@users.noreply.github.com>
2025-02-02 10:10:59 +0530

    Upload Variables

---
+++ 5 - Variables/script.js
@@ -0,0 +1,25 @@
+let variable1 = 3;
+console.log(variable1); // output 3
+
+const calculation = 2 + 2;
+console.log(calculation); //4
+console.log(calculation + 2); //6
+
+const result = calculation + 2;
+console.log(result); // output 6
+
+const message = 'Hello!!';
+console.log(message); // output Hello!
+
+variable1 = 5;
+console.log(variable1); // output 5 new value (5)
+
+variable1 = variable1 + 1;
+console.log(variable1); // output 6
+
+const variable2 = 3;
+var variable3 = 3;
+
+console.log(typeof message);
+console.log(typeof 3);
+console.log(typeof result);
