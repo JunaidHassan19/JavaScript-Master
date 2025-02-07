6fbae69faddb2b89d7b87bd1c18c1bbc239c0045
Junaid Hassan <123434178+JunaidHassan19@users.noreply.github.com>
2025-02-07 12:39:37 +0530

    Upload (Part 2) Advanced Functions

---
+++ 12b - (Part 2) Advanced Functions/Advance - Rock-Paper-Scissors/rock-paper-scissors.js
@@ -0,0 +1,128 @@
+let score = JSON.parse(localStorage.getItem('score'));
+
+    if(score === null) {
+      score = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+      };
+    }
+
+    updateScoreElement();
+    let isAutoPlaying = false;
+    let intervalId;
+
+    // const autoplay = () => {
+
+    // };
+    function autoplay() {
+      if(!isAutoPlaying) {
+        intervalId = setInterval(() => { // changed
+          const playerMove = pickComputerMove();
+          playGame(playerMove);
+        }, 1000);
+        isAutoPlaying = true;
+
+      } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+      }
+    }
+//===changed===
+    document.querySelector('.js-rock-button').addEventListener('click', () => {
+      playGame('rock');
+    });
+
+    document.querySelector('.js-paper-button').addEventListener('click', () => {
+      playGame('paper');
+    });
+
+    document.querySelector('.js-scissors-button').addEventListener('click', () => {
+      playGame('scissors');
+    });
+
+    document.body.addEventListener('keydown', (event) => {
+      if(event.key === 'r') {
+        playGame('Rock');
+      } else if (event.key === 'p') {
+        playGame('Paper');
+      } else if (event.key === 's') {
+        playGame('Scissors');
+      }
+    });
+///=========
+    function playGame(playerMove) {
+      const computerMove = (pickComputerMove());
+      let result = '';
+
+      if(playerMove === 'Scissors') {
+        if(computerMove === 'Rock') {
+          result = 'You lose.'; 
+        } else if (computerMove === 'Paper') {
+          result = 'You win.';
+        } else if (computerMove === 'Scissors') {
+          result = 'Tie.';
+        }
+
+      } else if(playerMove === 'Paper') {
+        if(computerMove === 'Rock') {
+          result = 'You win.'; 
+        } else if (computerMove === 'Paper') {
+          result = 'Tie.';
+        } else if (computerMove === 'Scissors') {
+          result = 'You lose.';
+        }
+        
+      } else if(playerMove === 'Rock') {
+          if(computerMove === 'Rock') {
+          result = 'Tie.'; 
+        } else if (computerMove === 'Paper') {
+          result = 'You lose.';
+        } else if (computerMove === 'Scissors') {
+          result = 'You win.';
+        }
+      }
+
+
+      if(result === 'You win.') {
+        score.wins += 1;
+      } else if(result === 'You lose.') {
+        score.losses += 1;
+      } else if(result === 'Tie.') {
+        score.ties += 1;
+      }
+
+      localStorage.setItem('score', JSON.stringify(score));
+
+      updateScoreElement();
+
+      document.querySelector('.js-result')
+       .innerHTML = result;
+
+      document.querySelector('.js-moves')
+        .innerHTML = `You <img src="images/${playerMove}-emoji.png" class="move-icon">
+    computer <img src="images/${computerMove}-emoji.png" class="move-icon">`;
+    }
+     
+    //   alert(`You picked ${playerMove}. computer picked ${computerMove}. ${result}
+    //   wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties}`);
+    // }
+
+    function updateScoreElement () {
+      document.querySelector('.js-score')
+      .innerHTML =`wins: ${score.wins} - losses: ${score.losses} - ties: ${score.ties}`;
+    }
+
+
+    function pickComputerMove () {
+      const randomNum =  Math.random();
+      if(randomNum >= 0 && randomNum < 1 / 3) {
+         computerMove = ('Rock');
+      } else if (randomNum >= 1 / 3 && randomNum < 2 / 3) {
+         computerMove= ('Paper');
+      } else if (randomNum >= 2 / 3 && randomNum < 1) {
+         computerMove = ('Scissors');
+      }
+        
+         return computerMove; 
+    }
---
+++ 12b - (Part 2) Advanced Functions/Advance - Todo List/todo-list.js
@@ -0,0 +1,56 @@
+const todoList = [{
+  name: 'Code',
+  dueDate: '2024-10-21'
+}, {
+  name: 'Sleep',
+  dueDate: '2024-12-21'
+}];
+
+renderTodoList ();
+
+function renderTodoList() {
+  let todoListHTML = ``;
+  
+  todoList.forEach((todoObject, index) => {  // changed
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+        <button class="delete-todo-button
+         js-delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  });
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deletebutton, index) => {
+      deletebutton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
+  }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addtodo();
+});
+
+function addtodo () {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name, 
+    // dueDate: dueDate
+    name,    //Shorthand property
+    dueDate
+  });
+  inputElement.value = '';
+
+  renderTodoList();
+}
